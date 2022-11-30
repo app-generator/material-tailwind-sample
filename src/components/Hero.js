bb446f303c44cb2429cd7dd37cf578b8286b0dfd
App Generator <51070104+app-generator@users.noreply.github.com>
2022-11-30 13:34:05 +0200

    Update HERO Section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,18 +7,16 @@ const Hero = () => {
 	return (
 		<div className='w-full lg:p-8 px-4 flex items-center justify-between min-h-[80vh] heroContainer '>
 			<div className='lg:w-[60%] w-full lg:px-6 lg:pr-14'>
-				<Typography className='text-3xl font-extrabold font-poppins mb-4'>
-					Create a great circle of friends
+				<Typography className='text-2xl font-extrabold font-poppins mb-4'>
+					Free Material-Tailwind Sample
 				</Typography>
 				<Typography className='font-poppins mb-6'>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-					eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-					minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-					aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore.
+					Material Tailwind is a free and open-source UI library inspired by Material Design that provides a unique experience for developers working with React and Tailwind CSS.
+					<br />
+					Actively supported by <a target="_blank" rel="noreferrer" href="https://bit.ly/3fKQZaL">Creative-Tim</a>.
 				</Typography>
 				<Button size='lg' color='purple'>
-					Get Connected
+					<a target="_blank" rel="noreferrer" href="https://github.com/app-generator/sample-material-tailwind">Download</a> 
 				</Button>
 			</div>
 			<div className='lg:w-[40%] w-full lg:block hidden '>
